75280345978b7a6655726c13471388edeeae01f0
Sarthak Patipati <pusarthak@gmail.com>
2024-04-16 16:46:28 -0500

    Created Swift project

---
+++ Nearby/Nearby/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nearby
+//
+//  Created by Sarthak Patipati on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nearby/Nearby/NearbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyApp.swift
+//  Nearby
+//
+//  Created by Sarthak Patipati on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct NearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
