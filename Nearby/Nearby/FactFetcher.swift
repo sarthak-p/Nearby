8f62eee72f5157fc8f85eb5b138f5c201d78e4ed
SaiSamarth123 <sai003sam@gmail.com>
2024-04-24 20:55:19 -0500

    fixed backend

--- Nearby/Nearby/FactFetcher.swift
+++ Nearby/Nearby/FactFetcher.swift
@@ -6,20 +6,40 @@
 //
 
 import Foundation
-
-struct Fact: Codable {
-    let title: String
-    let description: String
-}
+import Combine
 
 class FactFetcher: ObservableObject {
     @Published var facts: [Fact] = []
-    
+
+    private var apiKey: String {
+        guard let filePath = Bundle.main.path(forResource: "Config", ofType: "plist"),
+              let plist = NSDictionary(contentsOfFile: filePath),
+              let value = plist.object(forKey: "GeminiApiKey") as? String else {
+            fatalError("Couldn't find key 'GeminiApiKey' in 'Config.plist'.")
+        }
+        return value
+    }
+
     func loadFacts(latitude: Double, longitude: Double) {
-        // Ensure you handle your API URL and key correctly here
         guard let url = URL(string: "https://api.gemini.com/location/\(latitude),\(longitude)/facts") else { return }
         
         var request = URLRequest(url: url)
+        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
         
+        URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            if let data = data {
+                do {
+                    let decodedResponse = try JSONDecoder().decode([Fact].self, from: data)
+                    DispatchQueue.main.async {
+                        self?.facts = decodedResponse
+                    }
+                } catch {
+                    print("Decoding failed: \(error.localizedDescription)")
+                }
+            } else if let error = error {
+                print("Fetch failed: \(error.localizedDescription)")
+            }
+        }.resume()
     }
 }
+
--- Nearby/Nearby/LocationManager.swift
+++ Nearby/Nearby/LocationManager.swift
@@ -5,7 +5,6 @@
 //  Created by Sai Samarth Patipati Umesh on 4/22/24.
 //
 
-import Foundation
 import CoreLocation
 
 class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
@@ -15,16 +14,53 @@ class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
     override init() {
         super.init()
         locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // Set the desired accuracy of the location data
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        // Ask for the user's permission to access location data
         locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
     }
 
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        currentLocation = locations.first // or `.last` depending on your preference
+    func start() {
+        // Start updating the location
+        locationManager.startUpdatingLocation()
     }
     
+    func stop() {
+        // Stop updating the location
+        locationManager.stopUpdatingLocation()
+    }
+
+    // MARK: - CLLocationManagerDelegate Methods
+
+    // This method is called when the location manager has new location data
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // We're interested in the last location, which is the most recent
+        if let location = locations.last {
+            // Update the published currentLocation property
+            currentLocation = location
+        }
+    }
+    
+    // This method is called when there's an error obtaining the location
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Failed to find user's location: \(error.localizedDescription)")
+        // Handle error, e.g., by printing it to the console
+        print("Error obtaining location: \(error)")
+    }
+
+    // This method is called when the authorization status changes
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        // Check the authorization status and react accordingly
+        switch manager.authorizationStatus {
+        case .notDetermined, .restricted, .denied:
+            // Location access was denied or restricted
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            // Start location updates if the app is authorized
+            start()
+        default:
+            break
+        }
     }
 }
+
+
